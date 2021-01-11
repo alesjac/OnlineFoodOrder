7059fe4c68c600848c16298f60cbcebb073e3efe
alesjacani <alesjacani5@gmail.com>
2021-01-11 11:24:36 +0100

    :)

---
+++ src/main/java/com/ikubinfo/primefaces/managedbean/OrderManagedBean.java
@@ -0,0 +1,18 @@
+package com.ikubinfo.primefaces.managedbean;
+
+import javax.faces.bean.ManagedBean;
+
+import org.springframework.web.context.annotation.RequestScope;
+
+@ManagedBean(name="orderBean")
+@RequestScope
+public class OrderManagedBean {
+    
+	private boolean skip1,skip2,skip3,skip4,skip5,skip6;
+	
+
+	
+	
+	
+	
+}
