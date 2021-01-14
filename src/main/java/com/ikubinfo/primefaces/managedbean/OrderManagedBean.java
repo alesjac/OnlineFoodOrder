aea60c3d02ea4d1af3325fbb9084373189dab86d
alesjacani <alesjacani5@gmail.com>
2021-01-14 12:12:43 +0100

    guido2

--- src/main/java/com/ikubinfo/primefaces/managedbean/OrderManagedBean.java
+++ src/main/java/com/ikubinfo/primefaces/managedbean/OrderManagedBean.java
@@ -26,7 +26,7 @@ import com.ikubinfo.primefaces.util.Messages;
 @ViewScoped
 public class OrderManagedBean implements Serializable {
 	/**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = 1L;
 
